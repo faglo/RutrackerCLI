3cafbde59d41b846d96f4f9f0b39d8de61cd3bdb
Daniil Rakityansky <daniilrakityansky@MacBook-Pro-Daniil.local>
2021-08-23 16:18:10 +0300

    code-fix

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import webbrowser
 from rutracker_parser import RutrackerParser
 import os
 import argparse
+from typing import List
 
 LOAD_DIR = "./"
+MINI_SEP = "||"
+DEFAULT_SEP = "-"
 
 parser = argparse.ArgumentParser(description="Поиск и загрузка торрентов с Rutracker.org")
 parser.add_argument('-q', '--query', type=str, help="Поисковый запрос")
@@ -17,7 +20,7 @@ def get_strip():
     sz = 0
     cols = os.get_terminal_size().columns
     while sz != cols:
-        strip += '-'
+        strip += DEFAULT_SEP
         sz += 1
     return strip
 
@@ -32,6 +35,26 @@ def fill(text, size):
         return text[:size]
     else:
         return text
+
+
+def make_row(data: List[str]):
+    if args.minify:
+        sep = " {} ".format(MINI_SEP)
+        sb = ''
+        sb += fill(data[0], 2) + sep
+        sb += fill(data[2], os.get_terminal_size().columns - 80) + sep
+        sb += fill(data[3], 10) + sep
+        sb += fill(data[4], 5) + sep
+        sb += fill(data[5], 5) + sep
+        sb += fill(data[6], 5) + sep
+        sb += fill(data[7], 20) + sep
+        return sb
+    else:
+        sb = 'ID: {0}\nТопик: {1}\nНазвание: {2}\nРазмер: {3}\nСиды: {4}\nЛичи: {5}\nСкачано: {6} раз(а)\nСоздано: {7}'.format(
+            *data
+        )
+        sb += get_strip()
+        return sb
 
 
 def main():
@@ -52,51 +75,24 @@ def main():
 
 
 def search(query, start=0):
-    strip = get_strip()
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Поиск...")
     res = rp.search(query)
 
     if args.minify:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Поиск...")
-        res = rp.search(query, start)
-        print(
-            fill("#", 2),
-            fill("Название", os.get_terminal_size().columns - 80),
-            fill("Размер", 10),
-            fill("Сиды", 5),
-            fill("Личи", 5),
-            fill("Загружено", 5),
-            fill("Создано", 20),
-            sep=" || "
-        )
-        for i, result in enumerate(res['results']):
-            print(
-                fill(str(i), 2),
-                fill(result['torrent_name'], os.get_terminal_size().columns - 80),
-                fill(result['torrent_size'], 10),
-                fill(result['seeds'], 5),
-                fill(result['leeches'], 5),
-                fill(result['download_count'], 5),
-                fill(result['created_at'], 20),
-                sep=" || "
-            )
-        print("Страница {} из {}".format(res['start']//50, res['results_count']//50))
-    else:
-        print("Страниц: {} // Текущая страница: {}\n".format(round(res['results_count']/50), round(res['start']/50)))
-        for i, result in enumerate(res['results']):
-            print("ID: {}\nТопик: {}\nНазвание: {}\nРазмер: {}\nСиды: {}\nЛичи: {}\nСкачано: {} раз(а)\nСоздано: {}".format(
-                i,
-                result['topic_name'],
-                result['torrent_name'],
-                result['torrent_size'],
-                result['seeds'],
-                result['leeches'],
-                result['download_count'],
-                result['created_at']
-            ))
-            print(strip)
+        print(make_row(['#', 'Топик', 'Название', 'Размер', 'Сиды', 'Личи', 'Загружено', 'Создано']))
+
+    for i, result in enumerate(res['results']):
+        print(make_row([str(i),
+                        result['topic_name'],
+                        result['torrent_name'],
+                        result['torrent_size'],
+                        result['seeds'],
+                        result['leeches'],
+                        result['download_count'],
+                        result['created_at']
+                        ]))
+    print("Страница {} из {}".format(res['start']//50, res['results_count']//50))
 
     x = input("'d<ID>' загрузить '<ID>' открыть 'nn' след. 'pp' пред.: ")
     if 'd' in x:
